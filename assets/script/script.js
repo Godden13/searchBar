2a64c470bd1d0f159b0bd856516b3b1ca8c4d87a
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2022-12-14 00:02:59 +0100

    add project info

---
+++ assets/script/script.js
@@ -0,0 +1,65 @@
+const nameSearch = document.querySelector(".nameSearch");
+const userInfo = document.querySelector(".userInfo");
+
+function displayUser({name, age}){
+  return `<div class = "userInfo>
+          <div class = "dp">
+            <h>${name}</h>
+          <p>${age} year${age >1? "s": ""} old</p>
+          </div>
+          `
+}
+
+function displayUsers(persons) {
+  return persons.map(displayUser).join("")
+}
+
+function compareNames(name, searchTerm){
+  return name.toLowerCase().includes(searchTerm.toLowerCase());
+}
+
+function shouldResolve(){
+  return Math.random() < 0.85;
+}
+
+function searchUsers(name, age){
+  return new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      if(shouldResolve()) {
+        resolve(
+          users.filter(
+            (user) =>
+            (!name || compareNames(user.name, name)) &&
+            (!age || user.age === age)
+          )
+        );
+      } else {
+        reject([]);
+      }
+    }, 2000);
+  });
+}
+
+// function searchUsers(name, age) {
+//   return.filter(
+//     (user) => (!name || user.name === name) && (!ge || user.age === age)
+//   )
+// }
+
+userInfo.innerHTML = displayUsers(users)
+function renderMessage(message) {
+  return `<div class "message"> ${message}</div>`
+}
+nameSearch.addEventListener("submit", (e) => {
+  e.preventDefault()
+  userInfo.innerHTML = renderMessage("Searching users...")
+  searchUsers(e.target.name.value, +e.target.age.value)
+  .then((result) => {
+    userInfo.innerHTML = displayUsers((result));
+  })
+  .catch((e) => {
+    userInfo.innerHTML = renderMessage(
+      "Error Loading users! Please try again"
+    )
+  });
+});
---
+++ assets/script/users.js
@@ -0,0 +1,11 @@
+let users = [
+  {name: "Chriss Godden", age: 22},
+  {name: "Nsairun Emma", age: 31},
+  {name: "Mawiin Mulchas", age: 29},
+  {name: "Ngam Taryll", age: 21},
+  {name: "Christelle Pearl", age: 22},
+  {name: "Kun Aguero", age: 22},
+  {name: "Mungo Joy", age: 23},
+  {name: "Chia Carlyle", age: 26},
+  {name: "Che Gaston", age: 26},
+]
