33d731ee1126517054df4a3f63ce96dcb6542e08
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2022-12-14 14:57:05 +0100

    correct linter errors

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,43 +1,43 @@
-const nameSearch = document.querySelector(".name-search");
-const userInfo = document.querySelector(".user-info");
+const nameSearch = document.querySelector('.name-search');;
+const userInfo = document.querySelector('.user-info');;
 
-function displayUser({name, age}){
+function displayUser ({ name, age }) {
   return `<div class = "userInfo>
           <div class = "dp">
             <h>${name}</h>
-          <p>${age} year${age >1? "s": ""} old</p>
+          <p>${age} year${age > 1 ? 's' : ''} old</p>
           </div>
           `
 }
 
-function displayUsers(persons) {
-  return persons.map(displayUser).join("")
+function displayUsers (persons) {
+  return persons.map(displayUser).join('')
 }
 
-function compareNames(name, searchTerm){
-  return name.toLowerCase().includes(searchTerm.toLowerCase());
+function compareNames (name, searchTerm) {
+  return name.toLowerCase().includes(searchTerm.toLowerCase());;
 }
 
 function shouldResolve(){
-  return Math.random() < 0.85;
+  return Math.random() < 0.85;;
 }
 
-function searchUsers(name, age){
-  return new Promise((resolve, reject) =>{
+function searchUsers (name, age) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if(shouldResolve()) {
+      if (shouldResolve()) {
         resolve(
           users.filter(
-            (user) =>
+              (user) =>
             (!name || compareNames(user.name, name)) &&
             (!age || user.age === age)
           )
-        );
+        );;
       } else {
-        reject([]);
+        reject([]);;
       }
-    }, 2000);
-  });
+    }, 2000);;
+  });;
 }
 
 // function searchUsers(name, age) {
@@ -47,19 +47,19 @@ function searchUsers(name, age){
 // }
 
 userInfo.innerHTML = displayUsers(users)
-function renderMessage(message) {
+function renderMessage (message) {
   return `<div class "message"> ${message}</div>`
 }
-nameSearch.addEventListener("submit", (e) => {
+nameSearch.addEventListener('submit', (e) => {
   e.preventDefault()
-  userInfo.innerHTML = renderMessage("Searching users...")
+  userInfo.innerHTML = renderMessage('Searching users...')
   searchUsers(e.target.name.value, +e.target.age.value)
-  .then((result) => {
-    userInfo.innerHTML = displayUsers((result));
-  })
-  .catch((e) => {
-    userInfo.innerHTML = renderMessage(
-      "Error Loading users! Please try again"
-    )
-  });
-});
+    .then((result) => {
+      userInfo.innerHTML = displayUsers((result));;
+    })
+    .catch((e) => {
+      userInfo.innerHTML = renderMessage(
+        "Error Loading users! Please try again"
+      )
+    });;
+});;
